db3c4faef537476b4be7f6813386ef14e5e4c975
Gary Napier <garynapier@Garys-MacBook-Pro-2.local>
2020-10-04 16:49:47 +0100

    added images and explanation of prices

--- Andy_Hayler/app.R
+++ Andy_Hayler/app.R
@@ -1,14 +1,14 @@
 
 # TO DO
-# Change scatterplots to either 
-# pointsize = n data points
-# sample < 1 star
+
+
+# Summary (and plot) of all data
+  # Tables/ranges of each column
 
 # Clean locations data
 
-# Add notes
-
-# Label interesting points on scatterplot
+# Label interesting points on scatterplots
+  # Leading to breakdown of top/most interesting restaurants
 
 # --------------------------------------------------------------------------------
 
@@ -27,6 +27,8 @@ Round_up <- function(x,to=10)
 Remove_rows <- function(Data, Col, Value, N){
   Data[-(sample(as.numeric(row.names(subset(Data, Col == Value) )), N)), ]
 }
+
+fmt <- function(x, ...){format(x, big.mark=",",scientific=FALSE, ...)}
 
 library(XML)
 library(plyr)
@@ -120,14 +122,17 @@ France <- c(" Loire", " France")
 Tab$Country <- ifelse(Tab$Country %in% France, "France", Tab$Country)
 
 # ----------------
-# Table of stars
+# Data summary
 # ----------------
 
+n_reviews <- nrow(Tab)
 
-Star_tab <- table(Tab$Stars)
 
-Star_plot <- 
-  ggplot(Tab, aes(Stars))+
+
+# Table and barplot of reviews by star
+
+# Star_tab <- table(Tab$Stars)
+Star_plot <- ggplot(Tab, aes(Stars))+
   geom_histogram(bins = 4, alpha= 0.75, colour = "black", fill = "red")+
   xlab("Number Michelin stars")+ylab("Number of reviews")+
   # ylim(c(0, nrow(Tab)+100))+
@@ -163,35 +168,33 @@ Star_2 <- subset(Tab, Stars == 2)
 Star_3 <- subset(Tab, Stars == 3)
 
 
-Price_rate <- ggplot()
-Price_rate <- Price_rate + geom_point(data = Star_0, aes(Price, Rating, colour = factor(Stars),
-                                                         shape = factor(Stars)),
-                                      size = Size_0, alpha = Alpha, stroke = Stroke)
-Price_rate <- Price_rate + geom_point(data = Star_1, aes(Price, Rating, colour = factor(Stars),
-                                                         shape = factor(Stars)),
-                                      size = Size_1, alpha = Alpha, stroke = Stroke)
-Price_rate <- Price_rate + geom_point(data = Star_2, aes(Price, Rating, colour = factor(Stars),
-                                                         shape = factor(Stars)),
-                                      size = Size_2, alpha = Alpha, stroke = Stroke)
-Price_rate <- Price_rate + geom_point(data = Star_3, aes(Price, Rating, colour = factor(Stars),
-                                                         shape = factor(Stars)),
-                                      size = Size_3, alpha = Alpha, stroke = Stroke)+
-  stat_ellipse(data=Tab, aes(x = Price, y = Rating, colour = factor(Stars)), 
-               size = Circle_size, type = "t", show.legend = F, alpha = Alpha)+
-  scale_shape_manual(values=c(1, 1, 1, 1))+
-  theme_classic()+
-  ggtitle("Price vs Rating, grouped by star status")+
-  stat_smooth(data = Tab, se = T, formula = y~poly(x, 2), method = "lm", aes(x=Price, y=Rating),
-              show.legend = F)+
-  scale_colour_manual(values=cbPalette)+
-  scale_y_continuous(breaks = seq(0, 10, 1))+
-  scale_x_continuous(breaks = seq(0, max(Tab$Price), 50))+
-  theme(axis.title = element_text(size=Text_size),axis.text = element_text(size = Text_size),
-        legend.justification=c(1,0), legend.position=c(1,0))+
-  labs(shape = "Stars", colour = "Stars")+
-  guides(colour = guide_legend(override.aes = list(size=c(Size_0, Size_1, Size_2, Size_3))))
-
-
+# Price_rate <- ggplot()
+# Price_rate <- Price_rate + geom_point(data = Star_0, aes(Price, Rating, colour = factor(Stars),
+#                                                          shape = factor(Stars)),
+#                                       size = Size_0, alpha = Alpha, stroke = Stroke)
+# Price_rate <- Price_rate + geom_point(data = Star_1, aes(Price, Rating, colour = factor(Stars),
+#                                                          shape = factor(Stars)),
+#                                       size = Size_1, alpha = Alpha, stroke = Stroke)
+# Price_rate <- Price_rate + geom_point(data = Star_2, aes(Price, Rating, colour = factor(Stars),
+#                                                          shape = factor(Stars)),
+#                                       size = Size_2, alpha = Alpha, stroke = Stroke)
+# Price_rate <- Price_rate + geom_point(data = Star_3, aes(Price, Rating, colour = factor(Stars),
+#                                                          shape = factor(Stars)),
+#                                       size = Size_3, alpha = Alpha, stroke = Stroke)+
+#   stat_ellipse(data=Tab, aes(x = Price, y = Rating, colour = factor(Stars)), 
+#                size = Circle_size, type = "t", show.legend = F, alpha = Alpha)+
+#   scale_shape_manual(values=c(1, 1, 1, 1))+
+#   theme_classic()+
+#   ggtitle("Price vs Rating, grouped by star status")+
+#   stat_smooth(data = Tab, se = T, formula = y~poly(x, 2), method = "lm", aes(x=Price, y=Rating),
+#               show.legend = F)+
+#   scale_colour_manual(values=cbPalette)+
+#   scale_y_continuous(breaks = seq(0, 10, 1))+
+#   scale_x_continuous(breaks = seq(0, max(Tab$Price), 50))+
+#   theme(axis.title = element_text(size=Text_size),axis.text = element_text(size = Text_size),
+#         legend.justification=c(1,0), legend.position=c(1,0))+
+#   labs(shape = "Stars", colour = "Stars")+
+#   guides(colour = guide_legend(override.aes = list(size=c(Size_0, Size_1, Size_2, Size_3))))
 
 
 # # Change colors and shapes manually
@@ -200,8 +203,6 @@ Price_rate <- Price_rate + geom_point(data = Star_3, aes(Price, Rating, colour =
 #   scale_shape_manual(values=c(3, 16, 17))+
 #   scale_color_manual(values=c('#999999','#E69F00', '#56B4E9'))+
 #   theme(legend.position="top")
-
-
 
 
 # Price vs Rating (all)
@@ -349,6 +350,9 @@ Rating_cuisine <- ggplot(Tab, aes(Cuisine, Rating))+
   geom_hline(yintercept = median(Tab$Rating, na.rm = T),
              linetype = "dashed", colour = "red" )
 
+
+# 
+
 # # Rating by star
 # N_star <- data.frame(table(Tab$Stars))[,2]
 # Med_star <- c(by( Tab$Rating, Tab$Stars, median, na.rm = T))
@@ -405,39 +409,61 @@ ui <- fluidPage(
   
   mainPanel(
     
+    img(src="andy-hayler-logo.png"),
+    img(src="Andy_Hayler_photo.png",width='200px'),
+    
     h4(tags$div(
-      "Andy Hayler is an independent restaurant critic who has reviewed around 1500 restaurants
-      around the world.",
+      sprintf("Andy Hayler is an independent restaurant critic who has reviewed around %s restaurants
+      around the world.", fmt(n_reviews)),
       tags$br(),
       tags$br(),
-      "In five different years he ate in all restaurants in the world with three Michelin stars.
+      "In five different years he ate in all three Michelin star restaurants in the world.
       This is an exploration of the basic data from his reviews, including subjective rating, price,
       Michelin star status, location and cuisine type."
     )),
     
+    tags$br(),
+    tags$br(),
+
     h5(tags$div(
-      "A note on the ratings: these are the subjective ratings of Andy Hayler on his most recent
+      "A note on the ratings: These are the subjective ratings of Andy Hayler on his most recent
       visit to each restaurant.",
       tags$br(),
       tags$br(),
       "He scores them on a scale from 1 to 20. Previously, he rated restaurants from 1 to 10,
-      however this hasn't seemed to increased the range of scores because there are only about two
-      reviews with less than 10/20. Therefore ten was subtracted from all scores and I made any
-      scores less than zero just zero.",
+      however this hasn't seemed to increased the range of scores because there are very few 
+      reviews with less than 10/20. Therefore ten was subtracted from all scores and any
+      scores less than zero are rounded up to just zero. 
+      So the scores here have been adjusted back to a just a simple 1 to 10 scale.",
       tags$br(),
       tags$br(),
       "The range of restaurants is quite varied. There are reviews of
-      pizzerias and fish & chips alongside, say, a three-michelin star restaurant in the centre
+      pizzerias and fish & chips alongside, say, a three michelin star restaurant in the centre
       of Paris. But the criteria for review is generally that it's an independent place
       with some interesting reason to visit such as a prior recommendation or review",
       tags$br(),
       tags$br(),
-      "However, since Andy is from London, there are many more reviews of places in England.
-      It's quite rare that Andy marks a zero, and a score of 1-3 or 4 is actually pretty good
-      because it's relative to the extreme and rare values of 10/10 (see table below).",
+      "However, since Hayler is from London, there are many more reviews of places in England.
+      It's quite rare that he marks a zero, and a score of more than 2 or 3 means the food is actually pretty good
+      because the score is relative to the extreme and rare values of 10/10 (see table below).",
       tags$br(),
       tags$br(),
-      "Andy pays for the restaurants himself so there should be little bias here.")),
+      "Hayler pays for the restaurants himself so there should be little bias in the subjective score.")),
+    
+    h5(tags$div(
+      "A note on prices: The prices here are what he calls an 'average price', 
+      which is described as 'typical price for three courses and modest wine', 
+      however it's unclear how this is calculated exactly.",
+      tags$br(),
+      tags$br(),
+      "There can also be a big difference between this 'average price' and what Hayler himself pays,
+      especially at the top restaurants.
+      For example at Troisgros (10/10, 3 stars), 'average price' is 200, but the price he paid was 519, 
+      and at Les Pres Eugenie (also 10/10, 3 stars) 'average price' is 180 whereas Hayler paid ~600 (!).
+      Generally he either pays the average or more than the average. 
+      Details of what he pays are on each restaurant's review page."
+    )), 
+    
     
     h3("Number of reviews by star"),
     plotOutput("Star_plot"),
@@ -445,7 +471,7 @@ ui <- fluidPage(
     h3("Price vs rating"),
     plotOutput("Price_rate"),
     plotOutput("Price_rate_split"),
-    h3(tags$div("Note: Many of the one-star restaurants are obscured by the zero-star restaurants.
+    h6(tags$div("Note: Many of the one-star restaurants are obscured by the zero-star restaurants.
                 The circles highlight the star rating groups and are calculated using a
                 '95% confidence interval'",
                 tags$br(),
@@ -463,31 +489,31 @@ ui <- fluidPage(
                 "However it should be noted that there is more 'uncertainty' a the three-star level
                 (as indicated by the widening confidence interval) since they have the fewest reviews."
     )),
-    
+
     h3("Price vs value"),
     plotOutput("Price_val"),
     plotOutput("Price_val_split"),
-    
+
     h3("Rating vs value"),
     plotOutput("Rating_val"),
     plotOutput("Rating_val_split"),
-    
+
     h3("Data by country / cuisine"),
     plotOutput("Price_country_hist"),
     plotOutput("Rating_country"),
-    
+
     h4("Here we can see that the UK scores about 3 (median), while France and Germany
        are score top. I think the UK's low score can be explained by the fact that Andy reviews
        nearly everywhere he'll eat out, and being a native to the UK this will include
        a lot more casual / cheaper places.
        It's a bit surprising that Germany is higher than France,
        though there are a lot fewer reviews and a bit more spread."),
-    
+
     plotOutput("Rating_cuisine")
     
-    )
+    ) # mainPanel
   #    )
-    )
+    ) # fluidPage
 
 # Server output
 server <- function(input, output) {
@@ -496,13 +522,13 @@ server <- function(input, output) {
   
   output$Price_rate <- renderPlot({Price_rate})
   output$Price_rate_split <- renderPlot({Price_rate_split})
-  
+
   output$Price_val <- renderPlot({Price_val})
   output$Price_val_split <- renderPlot({Price_val_split})
-  
+
   output$Rating_val <- renderPlot({Rating_val})
   output$Rating_val_split <- renderPlot({Rating_val_split})
-  
+
   output$Price_country_hist <- renderPlot({Price_country_hist})
   output$Rating_country <- renderPlot({Rating_country})
   output$Rating_cuisine <- renderPlot({Rating_cuisine})
@@ -588,7 +614,7 @@ shinyApp(ui = ui, server = server)
 # sd(x)
 
 
-date()
+# date()
 # "Sun May 27 16:40:44 2018"
 
 
